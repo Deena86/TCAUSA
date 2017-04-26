33def22a3615c19410f51a11239f135dc40d7a48
Deenadayal Loganathan <deenadayalloganathan@deenadayals-air.fios-router.home>
2017-04-25 22:19:30 -0400

    Initial Commit

---
+++ TCAUSA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCAUSA
+//
+//  Created by Deenadayal Loganathan on 10/22/14.
+//  Copyright (c) 2014 Deena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCAUSA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TCAUSA
+//
+//  Created by Deenadayal Loganathan on 10/22/14.
+//  Copyright (c) 2014 Deena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
